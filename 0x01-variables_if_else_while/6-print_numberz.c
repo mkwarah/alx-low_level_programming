7bc274bc8c4c87c420a0b13d5e63e11c292d4b3e
Herrings <mkwara.h@gmail.com>
2022-10-14 15:33:42 -0700

    base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - base 10 number printing
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 18, i < 28, i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
