6f86417dafc3208ad3cd9b83072c989e83614e61
Herrings <mkwara.h@gmail.com>
2022-10-14 15:04:23 -0700

    printing numbers form 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing numbers form 0 to 9
+ * Return: 0
+ */
+inti main(void)
+{
+	int i;
+
+	for (i = 0; i < = 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
