f37d9cd42a531cbecdfd65f4e16a1d844a079c62
Herrings <mkwara.h@gmail.com>
2022-10-14 15:20:42 -0700

    printing alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printing alphabet except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
