119da3090fca5363e7416df05d6de900dac270c9
Herrings <mkwara.h@gmail.com>
2022-10-14 16:08:06 -0700

    hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - printing based 16 numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
