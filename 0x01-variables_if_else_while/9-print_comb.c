7c4837706811b1b51ceca79ff99b1f8713481e7e
Herrings <mkwara.h@gmail.com>
2022-10-14 16:43:09 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - printing possible combination of  a single digital numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int num[9] = "1,2,3,4,5,6,7,8,9";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d%d%d%d%d%d%d%d%d%d", num[i];
+	}
+	putchar('\n');
+	return (0);
+	}
