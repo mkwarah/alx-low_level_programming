3840cbd55e4a9cd3a3d032c2c4b2cfb917035fb0
Herrings <mkwara.h@gmail.com>
2022-10-18 14:48:07 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,18 +4,24 @@
  */
 void jack_bauer(void)
 {
-	int minute, hour;
+	int a, b, c, d;
 
-	for (hour = 0; hour <= 23; hour++)
+	for (a = 48; a <= 50; a++)
 	{
-		for (minute = 0; minute <= 59; minute++)
+		for (b = 48; b <= 57; b++)
 		{
-			_putchar((hour / 10) + '0'));
-			_putchar((hour % 10) + '0'));
-			_putchar(':');
-			_putchar((minute / 10) + '0'));
-			_putchar((minute % 10) + '0'));
-			_putchar('\n');
+			for (c = 48; c <= 53; c++)
+				for (d = 48; d <= 57; d++)
+				{
+					if (a >= 50 && b >= 52)
+						break;
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
 		}
 	}
 }
