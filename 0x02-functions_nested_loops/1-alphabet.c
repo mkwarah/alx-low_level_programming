a1a1f539bc57391141902aad2663ff1c33efa9d0
Herrings <mkwara.h@gmail.com>
2022-10-18 11:05:26 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - printing the alphabet in lower cases
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+
+	return (0);
+}
