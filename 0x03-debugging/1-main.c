be44a7347e056757538155ce51902bb6461d2881
Herrings <mkwara.h@gmail.com>
2022-10-19 02:32:39 -0700

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - infinite loop
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	prinntf("Infinite loop incoming :(\n");
+	i = 0;
+
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
