738d7bfc74edd9fed92784c9ae070de4322a4368
Herrings <mkwara.h@gmail.com>
2022-10-19 00:01:01 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,6 +9,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
+
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -20,7 +21,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
