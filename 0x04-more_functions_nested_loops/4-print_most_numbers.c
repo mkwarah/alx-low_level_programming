d4a128bbeb71fdc693146a0ec8488520571b1235
Herrings <mkwara.h@gmail.com>
2022-10-20 07:06:30 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
  * print_most_numbers - prints most numbers
  * Return: void
  */
-void print_most_numbers(void)
+/*void print_most_numbers(void)
 {
 	char c;
 
@@ -15,4 +15,41 @@ void print_most_numbers(void)
 		}
 	}
 			_putchar('\n');
+}*/
+#include "main.h"
+
+
+
+/**
+ *
+ * *print_most_numbers - prints
+ *
+ * * owned by Bwave ICT
+ *
+ * *Return: void
+ *
+ * */
+
+
+
+void print_most_numbers(void)
+
+{
+
+		char c;
+
+
+
+			for (c = '0'; c <= '9'; c++)
+
+					{
+
+							if (!(c == '2' || c == '4'))
+
+									_putchar(c);
+
+								}
+
+				_putchar('\n');
+
 }
