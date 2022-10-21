ada9683d2781a3362f18e803b3de5bcef5d86bde
Herrings <mkwara.h@gmail.com>
2022-10-21 02:04:23 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isupper - uppercase letters
  * @c: character to check
@@ -6,9 +7,11 @@
  * Return: 0 or 1
  */
 int _isupper(int c)
+
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
+
 }
